385c9791cc7c23baf933c1ba735bc84f36f9957c
Liu Yu Bao <liu@aisl.cs.tut.ac.jp>
2019-11-28 18:14:07 +0900

    Replace mark key C-SPC to C-c C-SPC

--- lisp/init-keybinding.el
+++ lisp/init-keybinding.el
@@ -1,10 +1,16 @@
+;; Description
+;;How to unbind key:
+;;; (global-set-key (kbd "C-e") nil)
+;;; (global-unset-key (kbd "C-e"))
+
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-e") 'move-end-of-line)
 
 ;(global-set-key (kbd "C-c C-,") 'org-insert-structure-template)
 
 ;;http://zhangley.com/article/emacs-ret/, donot band C-m to set-mark-command, because C-m is linked with <RET> key
-;(global-set-key (kbd "C-M-@") 'set-mark-command)
+(global-unset-key (kbd "C-SPC"))
+(global-set-key (kbd "C-c C-SPC") 'set-mark-command)
 
 ;(global-set-key (kbd "C-n") 'next-line)
 ;(global-set-key (kbd "C-p") 'previous-line)
